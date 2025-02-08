8245f51c13d919412d05b152235ffe68052d693c
Simon <brotherlogic@gmail.com>
2025-02-07 16:28:30 -0800

    Added ability to adjust file in overseer

--- go.mod
+++ go.mod
@@ -3,15 +3,15 @@ module github.com/brotherlogic/github_overseer
 go 1.23.5
 
 require (
-	github.com/brotherlogic/githubridge v0.2841.0
+	github.com/brotherlogic/githubridge v0.2843.0
 	github.com/brotherlogic/pstore v0.31.0
 	google.golang.org/grpc v1.70.0
-	google.golang.org/protobuf v1.36.4
+	google.golang.org/protobuf v1.36.5
 )
 
 require (
 	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250127172529-29210b9bc287 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250207221924-e9438ea467c6 // indirect
 )
--- task_tracker.go
+++ task_tracker.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"strings"
 
@@ -56,6 +57,8 @@ func trackTasks(ctx context.Context, repo string, config *pb.Config, client ghbc
 			return err
 		}
 	}
+
+	return nil
 }
 
 func processDocument(ctx context.Context, tDoc *pb.TrackedDocument, client ghbclient.GithubridgeClient) error {
@@ -92,5 +95,61 @@ func processDocument(ctx context.Context, tDoc *pb.TrackedDocument, client ghbcl
 		}
 	}
 
+	nfile, err := buildTasks(ctx, file, tasks, client)
+	if err != nil {
+		return err
+	}
+
+	// Write out nfile into the repo
+	log.Printf("WRITING %v", nfile)
+
 	return nil
+}
+
+func buildTasks(ctx context.Context, file string, tasks []*pb.Task, client ghbclient.GithubridgeClient) (string, error) {
+	for _, task := range tasks {
+		// We have a task entered for this one
+		if task.IssueId > 0 {
+			if task.State == pb.Task_TASK_STATE_IN_PROGRESS {
+				// Check to see if we've finished this task
+				issue, err := client.GetIssue(ctx, &ghbpb.GetIssueRequest{
+					User: "brotherlogic",
+					Repo: task.GetBouncedRepo(),
+					Id:   task.GetIssueId()})
+				if err != nil {
+					return "", err
+				}
+
+				if issue.GetState() == ghbpb.IssueState_ISSUE_STATE_CLOSED {
+					task.State = pb.Task_TASK_STATE_COMPLETE
+				}
+			}
+		}
+
+		// Replace the task line with the correct one given the state of the tasklist
+		replaceInFile(ctx, file, task.GetTask(), buildTask(task))
+	}
+
+	return file, nil
+}
+
+func buildTask(task *pb.Task) string {
+	strikeout := ""
+	if task.GetState() == pb.Task_TASK_STATE_COMPLETE {
+		strikeout = "~~"
+	}
+	return fmt.Sprintf("1. %v%v [%v%v#%v]%v", strikeout, task.GetTask(), "brotherlogic", task.GetBouncedRepo(), task.GetIssueId(), strikeout)
+}
+
+func replaceInFile(ctx context.Context, file, base, replace string) (string, error) {
+	var nlines []string
+	for _, line := range strings.Split(file, "\n") {
+		if strings.Contains(line, base) {
+			nlines = append(nlines, replace)
+		} else {
+			nlines = append(nlines, line)
+		}
+	}
+
+	return strings.Join(nlines, "\n"), nil
 }
